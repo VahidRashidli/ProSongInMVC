8b62c799bf0ae7f45a930282fdb701c01b9f6a1c
Vahid <vrashidli1@gmail.com>
2021-11-16 13:00:01 +0400

    Done

---
+++ ProSong/ProSong/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProSong.Models;
+
+namespace ProSong.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ContactController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Music()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
